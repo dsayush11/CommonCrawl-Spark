794ec396d36854703dd55bc518bde3a475f0f6dc
Rohit Ghatol <rohit.ghatol@synerzip.com>
2014-11-30 18:09:41 -0800

    Moved common classes to CommonCrawl-Common

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-GoogleAdsExplorer-Spark
+CommonCrawl-Spark
 =======================
 
 Google Ads Explorer Program which consumes data from Common Crawl to create report on Google Ads Usage. This program is a Apache Spark Program
--- src/main/java/com/synerzip/analytics/commoncrawl/googleads/counter/GoogleAdsCounter.java
+++ src/main/java/com/synerzip/analytics/commoncrawl/googleads/counter/GoogleAdsCounter.java
@@ -1,11 +1,24 @@
+/**
+ * Copyright (C) 2004-2014 Synerzip. 
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.synerzip.analytics.commoncrawl.googleads.counter;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -33,6 +46,11 @@ import com.synerzip.analytics.commoncrawl.googleads.parser.GoogleAdParser;
 
 
 public class GoogleAdsCounter  implements Serializable {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = -6609696095449768948L;
 
 	private static final Logger LOG = Logger.getLogger(GoogleAdsCounter.class);	
 	
